58139bb044691eca72d9733f0963653a5e1658f6
Alex Radnaev <alexander.radnaev@gmail.com>
2018-11-13 22:20:35 -0700

    commented out celery fix

--- etabotsite/etabotsite/settings.py
+++ etabotsite/etabotsite/settings.py
@@ -298,4 +298,4 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_DEFAULT_QUEUE = 'etabotqueue'
-CELERY_RESULT_BACKEND = None # Disabling the results backend
+CELERY_RESULT_BACKEND = None  # Disabling the results backend
