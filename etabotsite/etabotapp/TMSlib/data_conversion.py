60120302b41fd196749b06cb7f91e5df9ccd6789
Alex Radnaev <alexander.radnaev@gmail.com>
2018-11-29 23:04:06 -0700

    adding data conversion module

---
+++ etabotsite/etabotapp/TMSlib/data_conversion.py
@@ -0,0 +1,16 @@
+"""Collection of data conversion tools."""
+
+
+def get_velocity_json(velocities, project_name):
+    """Creates velocity json for UI from velocities python object
+    for a project named project_name"""
+    velocity = velocities.get(project_name)
+    velocity_json = {}
+    if velocity is not None:
+        velocity_json['mean'] = velocity.value
+        velocity_json['upper_estimate'] = velocity.higher_estimate()
+        velocity_json['lower_estimate'] = velocity.lower_estimate()
+    else:
+        logging.debug('velocity is None for project {}'.format(
+            project_name))
+    return velocity_json
