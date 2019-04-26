6226b2811dfd8db529eeb5c5df83c54dfc150b16
Alex Radnaev <alexander.radnaev@gmail.com>
2019-04-26 13:46:49 -0600

    updated rabbitmq config

--- rabbitmq/conf_rabbitmq.py
+++ rabbitmq/conf_rabbitmq.py
@@ -1,26 +1,25 @@
+"""Writes configuration file for RabbitMQ messenger."""
+
 import sys
 import json
+import logging
 
 with open('/custom_settings.json') as f:
-# with open('../etabotsite/custom_settings.json') as f:
     settings = json.load(f)
 
-rmq_user = settings['RMQ_USER']
-rmq_pass = settings['RMQ_PASS']
-# rmq_host = settings['RMQ_HOST']
-rmq_vhost = settings['RMQ_VHOST']
+rmq_user = settings.get('RMQ_USER')
+rmq_pass = settings.get('RMQ_PASS')
+rmq_vhost = settings.get('RMQ_VHOST')
 
-rabbitconf = """default_vhost = {vhost}
-default_user = {user}
-default_pass = {pword}
-""".format(
-    vhost=rmq_vhost, user=rmq_user, pword=rmq_pass)
+if rmq_user is not None and rmq_pass is not None and rmq_vhost is not None:
+    rabbitconf = """default_vhost = {vhost}
+    default_user = {user}
+    default_pass = {pword}
+    """.format(
+        vhost=rmq_vhost, user=rmq_user, pword=rmq_pass)
 
-with open('/etc/rabbitmq/rabbitmq.conf', 'w+') as f:
-    f.write(rabbitconf)
-
-# with open('/etc/hostname', 'w+') as f:
-#     f.write(rmq_host)
-
-# with open('/etc/hosts', 'a') as f:
-#     f.write('127.0.0.1 ' + rmq_host)
+    with open('/etc/rabbitmq/rabbitmq.conf', 'w+') as f:
+        f.write(rabbitconf)
+else:
+    logging.warning('not enough Rabbitmq settings. \
+Need RMQ_USER, RMQ_PASS, RMQ_VHOST in custom_settings.json')
