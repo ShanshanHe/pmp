64eaacaaabd3dc831869ef2b3bfcb31aea696eae
Alex Radnaev <alexander.radnaev@gmail.com>
2021-02-14 22:52:42 -0700

    updated JQL for more issue types

--- etabotsite/etabotapp/TMSlib/TMS.py
+++ etabotsite/etabotapp/TMSlib/TMS.py
@@ -159,7 +159,7 @@ cannot send connectivity issue email')
             assignee: str = None,
             project_names: List[str] = None,
             recent_time_period: str = None):
-        extra_filter = ' AND type != "Epic" '
+        extra_filter = ' AND (type = "Task" OR type = "Story") '
         if assignee is not None:
             extra_filter += 'AND assignee = {assignee}'.format(assignee=assignee)
 
