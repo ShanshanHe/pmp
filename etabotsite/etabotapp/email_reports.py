8f1ed6da38d11f0b03a1bbb40b65c92f3962c740
Alex Radnaev <alexander.radnaev@gmail.com>
2021-05-22 09:57:10 -0600

    added generation of empty report

--- etabotsite/etabotapp/email_reports.py
+++ etabotsite/etabotapp/email_reports.py
@@ -8,7 +8,7 @@ email reports to users.
 """
 import logging
 import socket
-
+import pandas as pd
 from typing import List, Dict
 
 from django.conf import settings
@@ -16,7 +16,7 @@ from django.template.loader import render_to_string
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import etabotapp.email_toolbox as email_toolbox
-from etabotapp.TMSlib.interface import HierarchicalReportNode
+from etabotapp.TMSlib.interface import HierarchicalReportNode, BasicReport
 
 SYS_DOMAIN = getattr(settings, "SYS_DOMAIN", "127.0.0.1")
 SYS_EMAIL = getattr(settings, "SYS_EMAIL", None)
@@ -38,8 +38,23 @@ class EmailReportProcess(object):
     def generate_html_report(user, reports: Dict[str, HierarchicalReportNode]):
         formatted_reports = []
         for project, project_report in reports.items():
+            project_formatted_reports = []
             for basic_report in project_report.all_reports():
-                formatted_reports.append(basic_report)
+                project_formatted_reports.append(basic_report)
+            if len(project_formatted_reports) == 0:
+                project_formatted_reports.append(BasicReport.empty_report(project))
+                email_toolbox.EmailWorker.send_email(email_toolbox.EmailWorker.format_email_msg(
+                    'no-reply@etabot.ai', 'hello@etabot.ai', 'no reports generated',
+                    'user: "{}" \n project: "{}"'.format(user.username, project)
+                ))
+
+            formatted_reports = formatted_reports + project_formatted_reports
+        if len(formatted_reports) == 0:
+            formatted_reports.append(BasicReport.empty_report('Something went wrong. Our apologies.'))
+            email_toolbox.EmailWorker.send_email(email_toolbox.EmailWorker.format_email_msg(
+                'no-reply@etabot.ai', 'hello@etabot.ai', 'no reports generated',
+                'user: "{}" \n projects: "{}"'.format(user.username, reports.keys())
+            ))
         msg_body = render_to_string('report_email.html', {
             'username': user.username,
             'reports': formatted_reports,
