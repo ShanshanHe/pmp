f8e8f24c8a978d1dd0e4497510ebfdb39de300c7
Alex Radnaev <alexander.radnaev@gmail.com>
2021-08-09 20:35:59 -0600

    added external logs

--- etabotsite/etabotapp/email_reports.py
+++ etabotsite/etabotapp/email_reports.py
@@ -8,8 +8,10 @@ email reports to users.
 """
 import logging
 import socket
+from datetime import datetime
+
 import pandas as pd
-from typing import List, Dict
+from typing import List, Dict, Tuple, Union
 
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -31,14 +33,33 @@ EMAIL_SUBJECT = '[ETAbot] Please verify your email'
 TOKEN_LINK = '{}/verification/activate/{}'
 
 
+def logs2html(logs: Union[None, List[Tuple[datetime, str]]]):
+    if logs is None:
+        return '<b>no logs available.</b>'
+    logs_html = '<table class="df_table">\n<tr><td>UTC</td><td>step time, s</td><td>log</td></tr>'
+    t1 = None
+    for log in logs:
+        t2 = log[0]
+        if t1 is not None:
+            dt = (t2 - t1).seconds
+        else:
+            dt = ''
+        logs_html += '\n<tr><td>{}</td><td>{}</td><td>{}</td></tr>'.format(
+            t2, dt, log[1])
+        t1 = t2
+    logs_html += '\n</table>'
+    return logs_html
+
+
 class EmailReportProcess(object):
     @staticmethod
     def send_email(msg):
         email_toolbox.EmailWorker.send_email(msg)
 
     @staticmethod
-    def generate_html_report(user, reports: Dict[str, HierarchicalReportNode]) -> (str, str):
+    def generate_html_report(user, reports: Dict[str, HierarchicalReportNode], logs=None) -> (str, str):
         formatted_reports = []
+        html_logs = logs2html(logs)
         for project, project_report in reports.items():
             project_formatted_reports = []
             for basic_report in project_report.all_reports():
@@ -52,10 +73,12 @@ class EmailReportProcess(object):
 
             formatted_reports = formatted_reports + project_formatted_reports
         if len(formatted_reports) == 0:
-            formatted_reports.append(BasicReport.empty_report('Something went wrong. Our apologies.'))
+            report = BasicReport.empty_report('Something went wrong. Our apologies.')
+            report.short_html = html_logs
+            formatted_reports.append(report)
             email_toolbox.EmailWorker.send_email(email_toolbox.EmailWorker.format_email_msg(
                 'no-reply@etabot.ai', 'hello@etabot.ai', 'no reports generated',
-                'user: "{}" \n projects: "{}"'.format(user.username, reports.keys())
+                'user: "{}" \n projects: "{}"\n logs: {}'.format(user.username, reports.keys(), html_logs)
             ))
         report_email = render_to_string('report_email.html', {
             'username': user.username,
@@ -65,7 +88,8 @@ class EmailReportProcess(object):
         full_report = render_to_string('full_report.html', {
             'username': user.username,
             'reports': formatted_reports,
-            'host': socket.gethostname()})
+            'host': socket.gethostname(),
+            'logs': html_logs})
 
         return report_email, full_report
 
--- etabotsite/etabotapp/eta_tasks.py
+++ etabotsite/etabotapp/eta_tasks.py
@@ -5,7 +5,7 @@ import etabotapp.TMSlib.TMS as TMSlib
 import logging
 import etabotapp.email_reports as email_reports
 
-from typing import List
+from typing import List, Tuple
 
 from etabotapp.TMSlib.interface import HierarchicalReportNode
 from etabotapp.models import TMS, Project
@@ -49,24 +49,36 @@ def estimate_ETA_for_TMS(
     add an option not to refresh velocities
     https://etabot.atlassian.net/browse/ET-521
     """
-
+    logs = []
     logger.debug(
         'estimate_ETA_for_TMS started for TMS {}, projects: {}'.format(
             tms, projects_set))
-    tms_wrapper = TMSlib.TMSWrapper(tms)
+    logs.append((datetime.utcnow(), 'TMS {} connectivity_status: {}'.format(str(tms), tms.connectivity_status)))
+    tms_wrapper = TMSlib.TMSWrapper(tms, logs=logs)
+    logs.append((datetime.utcnow(), 'tms wrapper initialized'))
     tms_wrapper.init_ETApredict(projects_set, **kwargs)
-
+    logs.append((datetime.utcnow(), 'ETA prediction module initialized'))
     project_names = save_project_velocities(tms_wrapper, projects_set)
+    logs.append((datetime.utcnow(), 'project names detected: {}'.format(project_names)))
 
     tms_wrapper.estimate_tasks(
         project_names=project_names,
+        logs=logs,
         **kwargs)
-
+    if tms_wrapper.ETApredict_obj is None:
+        logs.append((datetime.utcnow(), 'Error: ETApredict_obj is None'))
+    elif tms_wrapper.ETApredict_obj.df_tasks_with_ETAs is None:
+        logs.append((datetime.utcnow(), 'Error: df_tasks_with_ETAs is None'))
+    else:
+        logs.append((datetime.utcnow(), 'generated ETAs for {} tasks'.format(
+            tms_wrapper.ETApredict_obj.df_tasks_with_ETAs.shape[0])))
     raw_status_reports = tms_wrapper.generate_projects_status_report(
         project_names=project_names, **kwargs)
+    logs.append((datetime.utcnow(), 'generated {} status reports for: {}'.format(
+        len(raw_status_reports), ', '.join(list(raw_status_reports.keys())))))
 
     email_report, full_report = email_reports.EmailReportProcess.generate_html_report(
-        tms.owner, raw_status_reports)
+        tms.owner, raw_status_reports, logs=logs)
     images = []
     for report_name, report_node in raw_status_reports.items():
         for report in report_node.all_reports():
