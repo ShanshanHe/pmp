928e7b98fac11116aa25c16522519aa10d9600e3
Alex Radnaev <alexander.radnaev@gmail.com>
2022-10-14 00:17:22 -0600

    fixed circular import

--- etabotsite/etabotapp/views.py
+++ etabotsite/etabotapp/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .celery_tracking import send_celery_task_with_tracking
+from .celery_tracking import *
 from etabotapp.TMSlib.JIRA_API import update_available_projects_for_TMS
 from .serializers import UserSerializer, ProjectSerializer, TMSSerializer
 from .models import OAuth1Token, OAuth2Token, OAuth2CodeRequest
